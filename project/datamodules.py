7ef5c007cb91171432101244d9ffa9051f502543
abidikhairi <abidikhairi.1337@gmail.com>
2022-08-27 15:58:53 +0200

    project/datamodules.py

---
+++ project/datamodules.py
@@ -0,0 +1,67 @@
+import os
+import pytorch_lightning as pl
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import random_split, DataLoader
+
+
+class DataModule(pl.LightningDataModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.root_dir = os.environ['DATA_DIR']
+
+    def train_dataloader(self):
+        return DataLoader(self.trainset, batch_size=128, shuffle=True)
+
+    
+    def val_dataloader(self):
+        return DataLoader(self.validset, batch_size=128, shuffle=True)
+    
+
+    def test_dataloader(self):
+        return DataLoader(self.testset, batch_size=128, shuffle=False)
+
+
+class MNISTDataModule(DataModule):
+    
+    def setup(self, stage = None) -> None:
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+            transforms.Normalize(mean=(0.454, 0.455, 0.456), std=(0.551, 0.552, 0.553))
+        ])
+        
+        trainset = datasets.MNIST(root=self.root_dir, train=True, download=True, transform=transform)
+        testset = datasets.MNIST(root=self.root_dir, train=False, download=True, transform=transform)
+
+        valid_size = int(0.1 * len(trainset))
+
+        trainset, validset = random_split(trainset, [len(trainset) - valid_size, valid_size])
+
+        self.trainset = trainset
+        self.validset = validset
+        self.testset = testset
+
+
+    
+class CIFAR10DataModule(DataModule):
+    def setup(self, stage = None) -> None:
+
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.454, 0.455, 0.456), std=(0.551, 0.552, 0.553))
+        ])
+        
+        trainset = datasets.CIFAR10(root=self.root_dir, train=True, download=True, transform=transform)
+        testset = datasets.CIFAR10(root=self.root_dir, train=False, download=True, transform=transform)
+
+        valid_size = int(0.1 * len(trainset))
+
+        trainset, validset = random_split(trainset, [len(trainset) - valid_size, valid_size])
+
+        self.trainset = trainset
+        self.validset = validset
+        self.testset = testset
